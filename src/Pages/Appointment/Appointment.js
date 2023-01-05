2bef19de16072c9575f3f47e34dc80cdb15cadeb
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2023-01-05 19:52:27 +0600

    Working

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import chair from '../../assets/images/chair.png'
+import { format } from 'date-fns';
+import 'react-day-picker/dist/style.css'
+import { DayPicker } from 'react-day-picker';
 
 const Appointment = () => {
-    return (
-        <div>
-            
+    
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+                src={chair}
+                className="max-w-sm rounded-lg shadow-2xl"
+            />
+          <div className="bg-white rounded-2xl shadow-lg">
+            <DayPicker></DayPicker>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Appointment;
